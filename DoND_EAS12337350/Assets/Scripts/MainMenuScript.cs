cfbc5e75dbfc8acc52ed9036f74ba6eeaa786862
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2019-01-11 10:57:35 +0000

    Update MainMenuScript.cs

--- DoND_EAS12337350/Assets/Scripts/MainMenuScript.cs
+++ DoND_EAS12337350/Assets/Scripts/MainMenuScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//Nicholas Easterby		EAS12337350
 public class MainMenuScript : MonoBehaviour {
 
 	// Opens the game scene
